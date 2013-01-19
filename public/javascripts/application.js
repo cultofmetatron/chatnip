709d3cc727c916f598da0ac4381845f176543a1a
cultofmetatron <cultofmetatron@aumlogic.com>
2013-01-18 21:44:17 -0500

    added client side dependencies. now I just need to get the application loading javascript!

---
+++ public/javascripts/application.js
@@ -0,0 +1,12 @@
+//the application function that manages the main client side system
+
+var initialize = function() {
+    alert("hello there");
+
+}
+
+exports.initialize = initialize;
+
+
+
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,28 @@
+
+//the required client side dependencies
+
+require.config({
+    paths: {
+               jQuery: '/javascripts/libs/jquery',
+               Underscore: '/javascripts/libs/underscore',
+               Backbone: '/javascripts/libs/backbone',
+               text:      '/javascripts/libs/text.js',
+               Bootstrap: '/javascripts/libs/bootstrap',
+               templates: '../templates'
+           },
+
+    shim: {
+              'Bootstrap': ['jQuery'],
+              'Backbone' : ['jQuery', 'Underscore'],
+              'Application': ['Backbone']
+          }
+});
+
+require(['Application'], function(app) {
+    alert('hello');
+});
+
+
+
+
+
